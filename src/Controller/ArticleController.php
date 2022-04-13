e9a8811efb486ddaef4f445001f66471466d5bbe
Taikhun <taiyang78190@gmail.com>
2022-04-13 17:40:51 +0200

    creation of méthod createAction to practice deserializing Test with POSTMAN return 201 code OK --DONE

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Article;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,13 +13,17 @@ class ArticleController extends AbstractController
 {
     // this property is initialized here to be filled once the construct will be called 
     private $serializer;
+    private $deserializer;
 
     public function __construct(SerializerInterface $serializerI)
     {
         $this->serializer = $serializerI;
+        $this->deserializer = $serializerI;
     }
 
     /**
+     * Gets data that we set here for a SERIALIZE TEST
+     * 
      * @Route("/articles/{id}", name="article_show")
      */
     public function showAction()
@@ -39,5 +44,34 @@ class ArticleController extends AbstractController
         // dd($article);
 
         return $response;
+    }
+
+    /**
+     * Creates content that will be sent from user to API with POST method
+     * 
+     * @Route("/articles", name="article_create", methods={"GET","POST"})
+     * 
+     */
+    public function createAction(Request $request)
+    {
+        // retrieve http request data thanks to the method getContent calleb by Request object injected as parameters
+        $data = $request->getContent();
+        
+        // method get from Abstract controller allows to contain a service that can be injected, autowired
+        // Among the methods provided by Serializer we have the deserialize
+        $article = $this->deserializer->deserialize($data, 'App\Entity\Article', 'json');
+
+        // $em as Entity Manager is the service which provides methods to save data and send to database by using the ORM Doctrine
+        // We calll the manager first
+        $em = $this->getDoctrine()->getManager();
+
+        // Persisting is preparing for the saving
+        $em->persist($article);
+
+        // Flush send the saved data to database
+        $em->flush();
+
+        // HTTP_CREATED is the constant for code 201 displayed when response is well loaded on the user page
+        return new Response('', Response::HTTP_CREATED);
     }
 }
