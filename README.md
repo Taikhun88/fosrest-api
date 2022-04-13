93a7110e34791f949ae182ef8f898020346f662e
Taikhun <taiyang78190@gmail.com>
2022-04-13 15:56:04 +0200

    add comments and explantatins --DONE

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-##1st step  
+## 1st step  
 Install JMS serializer bundle with composer  
 On Symfony 4 version, check bundles.php in config folder and ADD   
-JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true]
-You can now use SerializerInterface in your method with construct or with injection dependancies
-
-##2nd step 
+JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true]  
+You can now use SerializerInterface in your method with construct or with injection dependancies  
+When using JMS this way you don't need to use it in services.yaml neither appkernel  
+ 
+## 2nd step 
 Install ORM Doctrine  
 Create the data base using the ORM Doctrine  
 Create the entity Article symfony console make:entity  
+Creation of Article Controller symfony console make:controller  
+
+
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,24 +10,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+    // this property is initialized here to be filled once the construct will be called 
     private $serializer;
 
     public function __construct(SerializerInterface $serializerI)
     {
         $this->serializer = $serializerI;
     }
+
     /**
      * @Route("/articles/{id}", name="article_show")
      */
     public function showAction()
     {
+        // Article entity provides the setters to create content for each property we added
         $article = new Article();
         $article
             ->setTitle('Mon premier article')
             ->setContent('Le contenu de mon article.');
 
+        // serializer private property is called here so we can now use its methods as SerializerInterface is injected as service
         $data = $this->serializer->serialize($article, 'json');
         
+        // Response object allows to set the headers content 
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
 
